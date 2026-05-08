20cec52093a028f7600f159834a64410099275df
suhanigoyal24 <gsuhani433@gmail.com>
2026-05-09 01:02:53 +0530

    Update config files

--- config/urls.py
+++ config/urls.py
@@ -10,3 +10,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""config/urls.py - Project-level URL configuration"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from config.views import home
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
